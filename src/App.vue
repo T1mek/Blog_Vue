febcf215745df29744b608db4912441333eb4cd4
T1mek <misha1234567890m@gmail.com>
2023-01-30 22:54:27 +0300

    Использован фунционал создание/удаления поста, REST API, сортировка

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,109 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="app">
+  <h1>Страница с постами</h1>
+      <div class="app_btns">
+            <my-button
+            @click="showDialog"
+            >Создание поста
+          </my-button>
+        <my-select
+            v-model="selectedSort"
+            :options="sortOptions"
+        />
+      </div>
+
+  <my-dialog v-model:show="dialogVisible">
+    <post-from
+        @create="createPost"
+    />
+  </my-dialog>
+  <post-list
+      :posts="sortedPosts"
+      @delete="deletePost"
+      v-if="!isLoading"
+
+  />
+  <div v-else>
+    <h2 >Идет загрузка ...</h2>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostFrom from "@/components/PostFrom";
+import PostList from "@/components/PostList";
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      posts:[],
+      dialogVisible : false,
+      modificatorValue:'',
+      isLoading:false,
+      selectedSort:'',
+      sortOptions:[
+          {value:'title',name:'По название'},
+          {value:'body',name:'По описание'},
+
+      ]
+    }
+  },
   components: {
-    HelloWorld
+PostFrom,PostList,
+  },
+  methods:{
+    createPost(post){
+      this.posts.push(post)
+      this.dialogVisible = false
+    },
+    deletePost(post){
+      this.posts = this.posts.filter( p => p.id !== post.id)
+    },
+    showDialog(){
+      this.dialogVisible=true
+    },
+    async fetchPosts(){
+
+      try {
+        this.isLoading = true
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        this.posts = response.data
+      }
+      catch (e) {
+        console.log('Fail')
+      }finally {
+        this.isLoading =false
+      }
+    },
+
+  },
+  mounted() {
+    this.fetchPosts()
+  },
+  computed:{
+    sortedPosts(){
+      return [...this.posts].sort((post1,post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+    }
+  },
+  watch:{
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app{
+  padding: 20px;
+}
+.app_btns{
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div v-if="posts.length > 0">
+    <h3>Список постов</h3>
+    <transition-group name="post-list">
+    <post-item
+      v-for="post in posts"
+      :key="post.id"
+      :post="post"
+      @delete="$emit('delete',post)"
+    />
+    </transition-group>
+  </div>
+  <h2 style="color: red" v-else>Постовов не найдено</h2>
+
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+export default {
+  components:{
+    PostItem
+  },
+  name: "PostList",
+  props:{
+    posts:{
+      type:Array,
+      required:true
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+.post-list-item{
+  display: inline-block;
+  margin-top: 10px;
+}
+.post-list-move,
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.5s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+</style>
